27a9e1be650f880b3339c9e4abf5e5138fb863f5
unknown <luceroyew@gmail.com>
2020-11-09 20:48:18 -0300

    Add the animals

--- temaiken/src/main/scala/domain/Animal.scala
+++ temaiken/src/main/scala/domain/Animal.scala
@@ -4,7 +4,40 @@ trait Animal {}
 
 object Animal {
 
-  case class Tigre() extends Animal
-  case class Aguila() extends Animal
+  sealed trait Mammal extends Animal
+  sealed trait Feline extends Mammal
+
+  case class Lion(
+      name: String
+  ) extends Feline
+  case class Tiger(
+      name: String
+  ) extends Feline
+
+  case class Puma(
+      name: String
+  ) extends Feline
+
+  case class Delphin(
+      name: String
+  ) extends Mammal
+
+  sealed trait Bird extends Animal
+
+  case class Pinguin(
+      name: String
+  ) extends Bird
+  case class Guacamaya(
+      name: String
+  ) extends Bird
+
+  sealed trait Reptilian extends Animal
+
+  case class Tortoise(
+      name: String
+  ) extends Animal
+  case class Lagarto(
+      name: String
+  ) extends Animal
 
 }
--- temaiken/src/main/scala/domain/Person.scala
+++ temaiken/src/main/scala/domain/Person.scala
@@ -3,21 +3,21 @@ package domain
 object Person {
 
   case class Person(
-  name: String,
-  location: Location,
-  genre : Genre,
-  age :Age
+      name: String,
+      location: Location,
+      genre: Genre,
+      age: Age
   )
   trait Genre
   case object Male extends Genre
   case object Female extends Genre
 
-  case class  Age (age:Int
-                   )
+  case class Age(age: Int)
 
+  case class Zooworkers(
+      name: String,
+      homework: String,
+      isInChargeOf: Animal
+  )
 
 }
-
-
-
-
